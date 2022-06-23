913d318a3951bd8dd57162fa163a92470ec0aaea
Jenny Chen <pelgrims.chenchingyi@gmail.com>
2022-06-23 23:34:43 +0200

    front with radis cache

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\QuestionRepository;
 use Psr\Cache\CacheItemInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,17 +13,13 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(HttpClientInterface $httpClient,CacheInterface $cache): Response
+    public function index(QuestionRepository $questionRepository): Response
     {
-        $mixedData= $cache->get('QnA', function(CacheItemInterface $cacheItem)use($httpClient){
-            $response=$httpClient->request('GET','');
-            $mix=$response->toArray();
-            $cacheItem->expiresAfter(3600);
-
-        });
-
-        return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
+ 
+        $response= $this->render('homepage/index.html.twig', [
+            'questions' => $questionRepository->findAll(),
         ]);
+        $response->setSharedMaxAge(3600);
+        return $response;
     }
 }
